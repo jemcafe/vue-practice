bffc571dc49a3b85cbb3f99d45e3033754b6e45d
Jemaine Brown <jembrown@Jemaines-MacBook-Pro.local>
2018-04-12 21:44:20 -0500

    Routing

--- src/router.js
+++ src/router.js
@@ -13,7 +13,7 @@ export default new Router({
             component: Skills
         },
         {
-            path: '/about',
+            path: '/about/:name',
             names: 'about',
             component: About
         },
