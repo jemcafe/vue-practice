6d076c2a2435dba4f23be25767d7eafa0ac8b85c
Jemaine Brown <jembrown@Jemaines-MacBook-Pro.local>
2018-04-12 20:32:22 -0500

    Methods and form

--- src/App.vue
+++ src/App.vue
@@ -24,4 +24,22 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 } */
+
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
+#app {
+    width: 50%;
+}
 </style>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="hello">
-    <!-- {{}} interpolation -->
-    {{ name }}
-    <!-- v-bind:disabled  (Disables the button) -->
-    <button v-bind:disabled="btnState">Change Name</button>
+    <div class="basics">
+      <!-- {{}} interpolation -->
+      {{ name }}
 
-    <div class="holder">
+      <!-- v-bind:disabled  (Disables the button) -->
+      <button v-bind:disabled="btnState">Change Name</button>
+
       <ul>
         <!-- looping -->
-        <li v-for="(data, index) in skills" :key="index">{{ index }}. {{ data.skill }}</li>
+        <!-- <li v-for="(data, index) in skills" :key="index">{{ index }}. {{ data.skill }}</li> -->
       </ul>
 
       <!-- if / else -->
@@ -21,9 +22,20 @@
 
       <!-- style binding -->
       <div v-bind:style="{ backgroundColor: bgColor, width: bgWidth, height: bgHeight }"></div>
+    </div>
 
-      <!--  -->
-      <p>The skills you posses</p>
+    <!--  -->
+    <div class="holder">
+
+      <form @submit.prevent="addSkill">  <!-- the modifier .prevent is basically .prevetDefault() -->
+        <input type="text" placeholder="Enter a skill..." v-model="skill">
+      </form>
+
+      <ul>
+        <li v-for="(data, index) in skills" :key="index">{{ data.skill }}</li>
+      </ul>
+
+      <p>The skills you possess</p>
     </div>
   </div>
 </template>
@@ -35,10 +47,6 @@ export default {
     return {
       name: 'Bumble',
       btnState: true,
-      skills: [
-        { "skill": "Vue.js" },
-        { "skill": "Front End Developer" },
-      ],
       showAlert: true,
       showClass: true,
       alertObject: {
@@ -47,7 +55,19 @@ export default {
       },
       bgColor: 'lightblue',
       bgWidth: '100%',
-      bgHeight: '30px'
+      bgHeight: '30px',
+
+      skills: [
+        { "skill": "Vue.js" },
+        { "skill": "Front End Developer" },
+      ],
+      skill: ''
+    }
+  },
+  methods: {
+    addSkill: function() {
+      this.skills.push({ skill: this.skill });
+      this.skill = '';
     }
   }
 }
@@ -56,12 +76,48 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 /* <style src="./Skills.css" scoped> */  /* Importing css */
+  .basics {
+    background: rgb(234, 255, 234); }
   .alert {
     background-color: lightblue;
     width: 100%;
-    height: 30px;
-  }
+    height: 30px; }
   .another-class {
-    border: 2px dashed grey;
+    border: 2px dashed grey; 
+    box-sizing: border-box; }
+
+  /* ------------ */
+  .holder {
+    margin-top: 20px;
+    background: #fff;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
   }
 </style>
